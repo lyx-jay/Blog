5ae093556f8b6e78037894423967d13bf57eac00
Lyx <1498781418@qq.com>
2022-07-14 21:26:24 +0800

    feat: init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,39 @@
+export default {
+  title: "VitePress",
+  description: "Write My Mind",
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/lyx-jay' },
+      { text: '掘金', link: '/' },
+      { text: '思否', link: '/' },
+    ],
+    sidebar: [
+      {
+        text: "WEBPACK",
+        items: [
+          {text: "webpack1", link: '/handbook/webpack/webpack1.md'}
+        ]
+      },
+      {
+        text: "REACT",
+        items: [
+        ]
+      },
+      {
+        text: "VUE",
+        items: [
+        ]
+      },
+      {
+        text: "VITEST",
+        items: [
+        ]
+      },
+      {
+        text: "PROJECTS",
+        items: [
+        ]
+      },
+    ]
+  }
+}
