187d8750f75104a5ab861c96b9da2b1ee4507729
Lyx <1498781418@qq.com>
2022-08-26 11:44:26 +0800

    feat: auto generate config

---
+++ docs/.vitepress/auto-generate-config.js
@@ -0,0 +1,62 @@
+
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * 
+ * @param {Object} config sidebar为空的config对象
+ * @param {String} rootfolderPath 存放文章的最外层目录
+ * @returns Object
+ */
+const auto_generate_config = function (config, rootfolderPath) {
+  const newConfig = JSON.parse(JSON.stringify(config));
+  const fileFolderNames = fs.readdirSync(rootfolderPath);
+  const folderNames = [];  // 所有文件夹名称
+
+  // 获取根目录下的所有文件夹名称
+  fileFolderNames.forEach(item => {
+    const location = path.join(rootfolderPath, item);
+    const info = fs.statSync(location);
+    if (info.isDirectory()) {
+      folderNames.push(item);
+    }
+  })
+
+  // 为每一个文件夹创建sidebar
+  folderNames.forEach(folder => {
+    newConfig.themeConfig.sidebar.push(
+      {
+        text: folder,
+        collapsed: true,
+        collapsible: true,
+        items: []
+      }
+    )
+  })
+
+  folderNames.forEach(folder => {
+    // 获取子文件夹的路径
+    const folderPath = rootfolderPath + '/' + folder;
+    // 检测当前子目录下的所有文件
+    const files = fs.readdirSync(folderPath);
+    files.forEach(file => {
+      const name = file;
+      const filePath = folderPath + '/' + file;
+      // 将文件名和其路径添加到sidebar对应的items中
+      newConfig.themeConfig.sidebar.forEach(sidebar => {
+        if (sidebar.text === folder) {
+          sidebar.items.push({
+            text: name,
+            link: filePath
+          });
+          return;
+        }
+      })
+    })
+  });
+
+  console.log(newConfig)
+  return newConfig;
+}
+
+export default auto_generate_config;
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,95 +1,111 @@
-export default {
+
+import auto_generate_config from "./auto-generate-config.js";
+
+const ROOTFOLDERPATH = '../handbook';
+
+const config = {
   title: "VitePress",
   description: "Write My Mind",
   base: "/Blog",
   themeConfig: {
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/lyx-jay' },
-      { text: '掘金', link: '/' },
-      { text: '思否', link: '/' },
-    ],
-    sidebar: [
-      {
-        text: "JavaScript基础",
-        collapsible:true,
-        collapsed:true,
-        items: [
-          {text: '图片懒加载', link:'/handbook/js/img_lazy_load.md'},
-          {text: '高频输入事件优化', link: '/handbook/js/high_frequency_input.md'},
-          {text: 'indexedDB封装思考', link: '/handbook/js/indexddb.md'},
-          {text: 'localStorage封装', link: '/handbook/js/localStorage.md'},
-        ]
-      },
-      {
-        text: "WEBPACK",
-        collapsible: true,
-        collapsed: true,
-        items: [
-          {text: "webpack1", link: '/handbook/webpack/webpack1.md'}
-        ]
-      },
-      {
-        text: "REACT",
-        collapsible: true,
-        collapsed: true,
-        items: [
-          {text: "JSX", link: "/handbook/react/JSX.md"},
-          {text: "Context", link: "/handbook/react/Context.md"},
-          {text: "Higher-Order Components", link: "/handbook/react/HOC.md"},
-          {text: "ErrorBoundary", link: "/handbook/react/ErrorBoundary.md"},
-          {text: "Uncontrolled Components", link: "/handbook/react/Uncontrolled_Components.md"},
-          {text: "组件内容补充", link: "/handbook/react/Components_supplement.md"},
-          {text: "React 架构设计", link: "/handbook/react/React_Architecture.md"},
-          {text: "Redux", link: "/handbook/react/Redux.md"},
-        ]
-      },
-      {
-        text: "VUE",
-        collapsible: true,
-        collapsed: true,
-        items: [
-          {text: "MVVM & MVC", link: '/handbook/vue/MVVM_MVC.md'},
-          {text: "Life Circle", link: '/handbook/vue/Vue_life_circle.md'},
-          {text: "Responsive Principle", link: '/handbook/vue/Vue_Responsive_Principle.md'},
-          {text: "Components Communication", link: '/handbook/vue/Vue_Components_Communication.md'},
-          {text: "Computed Property", link: '/handbook/vue/Computed_watcher.md'},
-          {text: "v-model", link: '/handbook/vue/V-Model.md'},
-        ]
-      },
-      {
-        text: "TYPESCRIPT",
-        collapsible: true,
-        collapsed: true,
-        items: [
-          {text: "原始类型与对象类型", link: '/handbook/typescript/original_object_type.md'}
-        ]
-      },
-      {
-        text: "PROJECTS",
-        collapsible: true,
-        collapsed: true,
-        items: [
-          {text: "Artist", link: '/handbook/Projects/Artist.md'},
-          {text: "reactuse", link: '/handbook/Projects/reactuse.md'}
-        ]
-      },
-      {
-        text: "网络基础知识",
-        collapsible: true,
-        collapsed: true,
-        items: [
-          {text: "使用Promise封装Ajax", link: '/handbook/basic/promise_ajax.md'},
-        ]
-      },
-      {
-        text: "常见CSS实例",
-        collapsible: true,
-        collapsed: true,
-        items: [
-          {text: "CSS-横向瀑布流布局", link: '/handbook/css/horizontal_waterfall.md'},
-          {text: 'stylelint-规范CSS', link: '/handbook/css/stylelint.md'}
-        ]
-      },
-    ]
+    sidebar: []
   }
 }
+
+export default auto_generate_config(config, ROOTFOLDERPATH);
+
+// export default {
+//   title: "VitePress",
+//   description: "Write My Mind",
+//   base: "/Blog",
+//   themeConfig: {
+//     socialLinks: [
+//       { icon: 'github', link: 'https://github.com/lyx-jay' },
+//       { text: '掘金', link: '/' },
+//       { text: '思否', link: '/' },
+//     ],
+//     sidebar: [
+//       {
+//         text: "JavaScript基础",
+//         collapsible:true,
+//         collapsed:true,
+//         items: [
+//           {text: '图片懒加载', link:'/handbook/js/img_lazy_load.md'},
+//           {text: '高频输入事件优化', link: '/handbook/js/high_frequency_input.md'},
+//           {text: 'indexedDB封装思考', link: '/handbook/js/indexddb.md'},
+//           {text: 'localStorage封装', link: '/handbook/js/localStorage.md'},
+//         ]
+//       },
+//       {
+//         text: "WEBPACK",
+//         collapsible: true,
+//         collapsed: true,
+//         items: [
+//           {text: "webpack1", link: '/handbook/webpack/webpack1.md'}
+//         ]
+//       },
+//       {
+//         text: "REACT",
+//         collapsible: true,
+//         collapsed: true,
+//         items: [
+//           {text: "JSX", link: "/handbook/react/JSX.md"},
+//           {text: "Context", link: "/handbook/react/Context.md"},
+//           {text: "Higher-Order Components", link: "/handbook/react/HOC.md"},
+//           {text: "ErrorBoundary", link: "/handbook/react/ErrorBoundary.md"},
+//           {text: "Uncontrolled Components", link: "/handbook/react/Uncontrolled_Components.md"},
+//           {text: "组件内容补充", link: "/handbook/react/Components_supplement.md"},
+//           {text: "React 架构设计", link: "/handbook/react/React_Architecture.md"},
+//           {text: "Redux", link: "/handbook/react/Redux.md"},
+//         ]
+//       },
+//       {
+//         text: "VUE",
+//         collapsible: true,
+//         collapsed: true,
+//         items: [
+//           {text: "MVVM & MVC", link: '/handbook/vue/MVVM_MVC.md'},
+//           {text: "Life Circle", link: '/handbook/vue/Vue_life_circle.md'},
+//           {text: "Responsive Principle", link: '/handbook/vue/Vue_Responsive_Principle.md'},
+//           {text: "Components Communication", link: '/handbook/vue/Vue_Components_Communication.md'},
+//           {text: "Computed Property", link: '/handbook/vue/Computed_watcher.md'},
+//           {text: "v-model", link: '/handbook/vue/V-Model.md'},
+//         ]
+//       },
+//       {
+//         text: "TYPESCRIPT",
+//         collapsible: true,
+//         collapsed: true,
+//         items: [
+//           {text: "原始类型与对象类型", link: '/handbook/typescript/original_object_type.md'}
+//         ]
+//       },
+//       {
+//         text: "PROJECTS",
+//         collapsible: true,
+//         collapsed: true,
+//         items: [
+//           {text: "Artist", link: '/handbook/Projects/Artist.md'},
+//           {text: "reactuse", link: '/handbook/Projects/reactuse.md'}
+//         ]
+//       },
+//       {
+//         text: "网络基础知识",
+//         collapsible: true,
+//         collapsed: true,
+//         items: [
+//           {text: "使用Promise封装Ajax", link: '/handbook/basic/promise_ajax.md'},
+//         ]
+//       },
+//       {
+//         text: "常见CSS实例",
+//         collapsible: true,
+//         collapsed: true,
+//         items: [
+//           {text: "CSS-横向瀑布流布局", link: '/handbook/css/horizontal_waterfall.md'},
+//           {text: 'stylelint-规范CSS', link: '/handbook/css/stylelint.md'}
+//         ]
+//       },
+//     ]
+//   }
+// }
